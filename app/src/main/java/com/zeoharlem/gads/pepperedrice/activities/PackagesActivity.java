743758a6f2f68931e2ce8a623ce04320b566e2a1
zeoharlem <zeoharlem@yahoo.co.uk>
2021-03-28 01:30:53 +0100

    PepperedRice Meals

---
+++ app/src/main/java/com/zeoharlem/gads/pepperedrice/activities/PackagesActivity.java
@@ -0,0 +1,16 @@
+package com.zeoharlem.gads.pepperedrice.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zeoharlem.gads.pepperedrice.R;
+
+public class PackagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages);
+    }
+}
